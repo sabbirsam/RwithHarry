6a76f84e4b3e019acd48fd4971140068d8447aba
sabbirsam <samiubat14@gmail.com>
2022-07-06 10:36:18 +0600

    react titlecase project

--- src/components/Texteditor.js
+++ src/components/Texteditor.js
@@ -5,16 +5,24 @@ export default function Texteditor( props) {
         let txts = text.toUpperCase();
         updateText( txts );
     }
+
+    const handleClick_case =()=>{
+      let txts = text.toLowerCase().replace(/\b\w/g, s => s.toUpperCase());
+      updateText( txts );
+  }
+
+
     const handleChange =(event)=>{
         updateText(event.target.value);
     }
-    const [text, updateText] = useState("what are you doing");
+    const [text, updateText] = useState("Write here what you want to capitalize");
   return (
    <div className="container">
      <div className="mb-3">
         <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.title}</label>
         <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={text} onChange={handleChange}></textarea>
         <button className="btn btn-primary my-3" onClick={handleClick}> Uppercase</button>
+        <button className="btn btn-primary my-3 mx-3" onClick={handleClick_case}>Titlecase</button>
     </div>
    </div>
   )
